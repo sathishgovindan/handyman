5905f6c0621e5680d630c38653ab9fb42aa60715
Sathish Govindan <sarathisathish@gmail.com>
2019-11-01 19:28:36 +0530

    Perfect code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
-
+import { HighlightModule } from 'ngx-highlightjs';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CodeGenComponent } from './component/code-gen/code-gen.component';
+import typescript from 'highlight.js/lib/languages/typescript';
+import javascript from 'highlight.js/lib/languages/javascript';
 
+export function hljsLanguages() {
+  return [
+    {name: 'typescript', func: typescript},
+    {name: 'javascript', func: javascript}
+  ];
+}
 
 @NgModule({
   declarations: [
@@ -15,7 +22,10 @@ import { CodeGenComponent } from './component/code-gen/code-gen.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,FormsModule 
+    AppRoutingModule,FormsModule,
+    HighlightModule.forRoot({
+      languages: hljsLanguages
+    }) 
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/component/code-gen/code-gen.component.ts
+++ src/app/component/code-gen/code-gen.component.ts
@@ -1,191 +1,181 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import * as _ from 'lodash';
 @Component({
   selector: 'app-code-gen',
   templateUrl: './code-gen.component.html',
   styleUrls: ['./code-gen.component.css']
 })
-export class CodeGenComponent implements OnInit {
+export class CodeGenComponent{
 
   textValue:string = '';
   handyResult:string = '';
   textOutValue:string = '';
-  lineNumber:number = 0;
   constructor() { 
    
   }
 
-  ngOnInit() {
+  /* To copy Text from Textbox */
+  copyInputMessage(inputElement){
+    inputElement.select();
+    document.execCommand('copy');
+    inputElement.setSelectionRange(0, 0);
+  }
+
+  /* To copy any Text */
+  copyText(){
+      let selBox = document.createElement('textarea');
+      selBox.style.position = 'fixed';
+      selBox.style.left = '0';
+      selBox.style.top = '0';
+      selBox.style.opacity = '0';
+      selBox.value = this.handyResult;
+      document.body.appendChild(selBox);
+      selBox.focus();
+      selBox.select();
+      document.execCommand('copy');
+      document.body.removeChild(selBox);
+  }
+
+  resetResult(){
+    this.handyResult = '';
+    this.textOutValue = '';
   }
 
   forLoop(type){
-    if(this.textValue != ''){
-      this.convertCamelCase();
-      let array:string = '';
-      let item:string = '';
-      if(this.textOutValue.substr(-1) != 's'){
-        array= this.textOutValue + 's';
-        item = this.textOutValue;
-      }
-      else{
-        array= this.textOutValue;
-        item = this.textOutValue.slice(0, -1);
-      }
-      if(type == 'of'){
-        this.handyResult = `for(let ${item} of ${array}){\n    //your logic \n}`;
-      }
-      else if(type == 'in'){
-        this.handyResult = `for(let index in ${array}){\n    //your logic \n}`;
-      }
-      else if(type == 'each'){
-        this.handyResult = `${array}.forEach(function (${item}, index, theArray) {\n    //your logic \n});`;
-      }
-      else{
-        this.handyResult = `for (index = 0; index < ${array}.length; ++index) {\n    //your logic \n}`;
-      }
+    this.resetResult();
+    if(this.textValue == '' || this.textValue == 'Your text' || this.textValue == 'yourObject'){
+      this.textValue = 'yourArrayItem'
     }
-    this.codeView();   
+    let array:string = '';
+    let item:string = '';
+    let textOutValue = _.camelCase(this.textValue);
+    if(textOutValue.substr(-1) != 's'){
+      array= textOutValue + 's';
+      item = textOutValue;
+    }
+    else{
+      array= textOutValue;
+      item = textOutValue.slice(0, -1);
+    }
+    if(type == 'of'){
+      this.handyResult = `for (let ${item} of ${array}){\n    //your logic \n}`;
+    }
+    else if(type == 'in'){
+      this.handyResult = `for (let index in ${array}){\n    //your logic \n}`;
+    }
+    else if(type == 'each'){
+      this.handyResult = `${array}.forEach(function (${item}, index, theArray) {\n    //your logic \n});`;
+    }
+    else{
+      this.handyResult = `for (index = 0; index < ${array}.length; ++index) {\n    //your logic \n}`;
+    }
+           
   }
 
   forOfObject(type){
-    if(this.textValue != ''){
-      this.convertCamelCase();      
-      if(type == 'Entries'){
-        this.handyResult = `for (const [key, value] of Object.entries(${this.textOutValue})) {\n    //your logic\n}`;
-      }
-      else if(type == 'Keys'){
-        this.handyResult = `for (const key of Object.keys(${this.textOutValue})) {\n    //your logic\n}`;
-      }
-      else{
-        this.handyResult = `for (const value of Object.values(${this.textOutValue})) {\n    //your logic\n}`;
-      }
+    this.resetResult();    
+    if(this.textValue == '' || this.textValue == 'yourArrayItem' || this.textValue == 'Your text'){
+      this.textValue = 'yourObject'
+    }  
+    let textOutValue = _.camelCase(this.textValue); 
+    if(type == 'Entries'){
+      this.handyResult = `for (const [key, value] of Object.entries(${textOutValue})) {\n    //your logic\n}`;
     }
-    this.codeView();   
+    else if(type == 'Keys'){
+      this.handyResult = `for (const key of Object.keys(${textOutValue})) {\n    //your logic\n}`;
+    }
+    else{
+      this.handyResult = `for (const value of Object.values(${textOutValue})) {\n    //your logic\n}`;
+    }          
   }
 
   forEachOfObject(type){
-    if(this.textValue != ''){
-      this.convertCamelCase();      
-      if(type == 'Entries'){
-        this.handyResult = `Object.entries(${this.textOutValue}).forEach(([key, value]) => { \n    //your logic\n});`;
-      }
-      else if(type == 'Keys'){
-        this.handyResult = `Object.keys(${this.textOutValue}).forEach((key) => { \n    //your logic\n});`;
-      }
-      else{
-        this.handyResult = `Object.values(${this.textOutValue}).forEach((value) => { \n    //your logic\n});`;
-      }
+    this.resetResult();
+    if(this.textValue == '' || this.textValue == 'yourArrayItem' || this.textValue == 'Your text'){
+      this.textValue = 'yourObject'
     }
-    this.codeView();   
+    let textOutValue = _.camelCase(this.textValue);
+    if(type == 'Entries'){
+      this.handyResult = `Object.entries(${textOutValue}).forEach(([key, value]) => { \n    //your logic\n});`;
+    }
+    else if(type == 'Keys'){
+      this.handyResult = `Object.keys(${textOutValue}).forEach((key) => { \n    //your logic\n});`;
+    }
+    else{
+      this.handyResult = `Object.values(${textOutValue}).forEach((value) => { \n    //your logic\n});`;
+    }           
   }  
 
-  convertSnakeCase(){
-    if(this.textValue != ''){  
-      this.textOutValue = this.textValue;   
-      this.textOutValue = _.snakeCase(this.textOutValue); 
+  addYourText(){
+    if(this.textValue == '' || this.textValue == 'yourArrayItem' || this.textValue == 'yourObject'){
+      this.textValue = 'Your text'
     }
+  }
+
+  convertSnakeCase(){
+    this.resetResult(); 
+    this.addYourText();  
+    this.textOutValue = this.textValue;   
+    this.textOutValue = _.snakeCase(this.textOutValue);     
   }
 
   convertLowerCase(){
-    if(this.textValue != ''){     
-      this.textOutValue = this.textValue;   
-      this.textOutValue = this.textOutValue.toLowerCase(); 
-    }
+    this.resetResult();
+    this.addYourText();         
+    this.textOutValue = this.textValue;   
+    this.textOutValue = this.textOutValue.toLowerCase();     
   }
 
   convertUpperCase(){
-    if(this.textValue != ''){     
-      this.textOutValue = this.textValue;
-      this.textOutValue = this.textOutValue.toUpperCase(); 
-    }
+    this.resetResult();
+    this.addYourText();
+    this.textOutValue = this.textValue;
+    this.textOutValue = this.textOutValue.toUpperCase();     
   }
 
   toPascalCase() {
-    if(this.textValue != ''){
-      this.textOutValue = this.textValue;
-      this.textOutValue =  `${this.textOutValue}`
-      .replace(new RegExp(/[-_]+/, 'g'), ' ')
-      .replace(new RegExp(/[^\w\s]/, 'g'), '')
-      .replace(
-        new RegExp(/\s+(.)(\w+)/, 'g'),
-        ($1, $2, $3) => `${$2.toUpperCase() + $3.toLowerCase()}`
-      )
-      .replace(new RegExp(/\s/, 'g'), '')
-      .replace(new RegExp(/\w/), s => s.toUpperCase());
-    }
+    this.resetResult();
+    this.addYourText();
+    this.textOutValue = this.textValue;
+    this.textOutValue =  `${this.textOutValue}`
+    .replace(new RegExp(/[-_]+/, 'g'), ' ')
+    .replace(new RegExp(/[^\w\s]/, 'g'), '')
+    .replace(
+      new RegExp(/\s+(.)(\w+)/, 'g'),
+      ($1, $2, $3) => `${$2.toUpperCase() + $3.toLowerCase()}`
+    )
+    .replace(new RegExp(/\s/, 'g'), '')
+    .replace(new RegExp(/\w/), s => s.toUpperCase());  
   }
 
   toHyphenCase(){
-    if(this.textValue != ''){
-      this.textOutValue = this.textValue;
-      this.textOutValue = this.textOutValue.replace(/ +/g, '-').toLowerCase();
-    }
+    this.resetResult();
+    this.addYourText();    
+    this.textOutValue = this.textValue;
+    this.textOutValue = this.textOutValue.replace(/ +/g, '-').toLowerCase();    
   }
 
   convertSentence(){
-    if(this.textValue != ''){
-      this.textOutValue = this.textValue;     
-      this.textOutValue = _.capitalize(this.textOutValue); 
-    }    
+    this.resetResult();
+    this.addYourText();
+    this.textOutValue = this.textValue;     
+    this.textOutValue = _.capitalize(this.textOutValue);     
   }  
 
   toTitleCase() {
-    if(this.textValue != ''){
-      let sentence = this.textValue.toLowerCase().split(" ");
-      for(var i = 0; i< sentence.length; i++){
-          sentence[i] = sentence[i][0].toUpperCase() + sentence[i].slice(1);
-      }
-      this.textOutValue = sentence.join(" ");      
+    this.resetResult();
+    this.addYourText();
+    let sentence = this.textValue.toLowerCase().split(" ");
+    for (var i = 0; i< sentence.length; i++){
+        sentence[i] = sentence[i][0].toUpperCase() + sentence[i].slice(1);
     }
+    this.textOutValue = sentence.join(" ");          
   }
 
   convertCamelCase(){
-    if(this.textValue != ''){     
-      this.textOutValue = this.textValue;
-      this.textOutValue = _.camelCase(this.textOutValue); 
-    }
-  }
-
-  counter(i: number) {
-    return new Array(i);
-  }
-
-  codeView() {
-    this.lineNumber = this.handyResult.split(/\n/).length;
-  }
-  
-  changeCodeFontSize(size) {
-    if (size == 0) { return }
-    var elements = document.getElementsByTagName('pre')
-    for (var i = 0; i < elements.length; i++) {
-      elements[i].style['font-size'] = size + 'px'
-    }
-  }
-  
-  codeCopy(id) {
-    var element = document.getElementById(id)
-    var text = element.innerHTML
-    //text.select
-    element.focus
-  
-    try {
-      document.execCommand('copy')
-      this.copyCodeToClipboard(text, id)
-    } catch (err) {
-      console.error('Something went wrong to copy to clipboard', err)
-    }
-  }
-  
-  copyCodeToClipboard(text, id) {
-    if (!navigator.clipboard) {
-      console.warn('Re-try')
-      this.codeCopy(id)
-      return
-    }
-    navigator.clipboard.writeText(text).then(function () {
-      // done
-    }, function (err) {
-      console.error('Could not copy text: ', err)
-    })
-  }
+    this.resetResult();
+    this.addYourText();
+    this.textOutValue = this.textValue;
+    this.textOutValue = _.camelCase(this.textOutValue);     
+  }  
 }
